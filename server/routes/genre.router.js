1103a522f2942737fa6947c7b9aecdb3bea193f6
Emma Song Fisher <emmasongf@gmail.com>
2020-11-06 20:20:30 -0600

    getMovies request made, movie posters displaying, movies with several genres displaying twice

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool')
 
-router.get('/', (req, res) => {
-  // Add query to get all genres
-  res.sendStatus(500)
-});
+// router.get('/', (req, res) => {
+//   // Add query to get all genres
+//   res.sendStatus(500)
+// });
 
 router.get('/', (req, res) => {
   const queryText = `SELECT * FROM "movies"
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,38 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 
 class Home extends Component {
+
+state = {
+    movieArray: []
+}
+
+
+componentDidMount = () => {
+    this.getMovies();
+}
+
+getMovies = () => {
+    axios({
+        method: 'GET',
+        url: '/api/genre'
+    }).then((response) => {
+        console.log(response.data);
+        this.setState({
+            movieArray: response.data
+        });
+        }).catch((error) => {
+            console.log(error)
+    })
+}
+
     render(){
         return(
             <div>
-            console.log('in home component')
+            {this.state.movieArray.map((poster) => {
+                return <button><img src={poster.poster}/></button>
+            })}
             </div>
         )
     }
