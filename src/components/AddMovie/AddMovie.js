40ae39780526703318e1f276bb93dca297ab698d
Emma Song Fisher <emmasongf@gmail.com>
2020-11-07 20:28:05 -0600

    inputs added to addMovie

--- src/components/AddMovie/AddMovie.js
+++ src/components/AddMovie/AddMovie.js
@@ -8,13 +8,51 @@ class AddMovie extends Component {
         this.props.history.push('/');
     }
 
+//     an input field (for the movie title)
+// an input field (for the movie poster image URL))
+// a textarea (for the movie description)
+// a dropdown (for the genres)
+
     render(){
         return(
-            <div>
+            <>
+            <div className="addMovie buttons">
             {/* // console.log('in addmovie component') */}
+          
+            </div>
+            
+            <form>
+            {/* <input>Movie title:</input> */}
+            {/* <input>Movie poster:</input> */}
+            <label for="title">Movie title:</label>
+            <input type="text" id="movieTitle" name="title"></input>
+            <br></br>
+            <label for="poster">Movie poster:</label>
+            <input type="text" id="moviePoster" name="poster"></input>
+            <br></br>
+            <textarea>Movie description:</textarea>
+            <br></br>
+            <label for="genres">Choose a genre:</label>
+            <select name="genres" id="genre">
+                <option value="adventure">Adventure</option>
+                <option value="animated">Animated</option>
+                <option value="biographical">Biographical</option>
+                <option value="comedy">Comedy</option>
+                <option value="disaster">Disaster</option>
+                <option value="drama">Drama</option>
+                <option value="epic">Epic</option>
+                <option value="fantasy">Fantasy</option>
+                <option value="musical">Musical</option>
+                <option value="romantic">Romantic</option>
+                <option value="science fiction">Science Fiction</option>
+                <option value="space-opera">Space-Opera</option>
+                <option value="superhero">Superhero</option>
+            </select>
+            <br></br>
             <button >Save</button>
             <button onClick={this.changePageBack}>Cancel</button>
-            </div>
+            </form>
+            </>
         )
     }
 }
